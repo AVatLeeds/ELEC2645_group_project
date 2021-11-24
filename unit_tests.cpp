cbf05e976d86b113890a04ae6f220d969af9dc32
Arthur Vie <arthur.vie@npl.co.uk>
2021-11-24 14:28:15 +0000

    Created a framework for writing unit tests

---
+++ unit_tests.cpp
@@ -0,0 +1,21 @@
+
+typedef enum
+{
+    PLACEHOLDER
+} test_t;
+
+test_t current_test;
+
+int main()
+{
+    switch (current_test)
+    {
+        case PLACEHOLDER:
+        break;
+
+        default:
+        break;
+    }
+    
+    return 0;
+}
