3c173cf00eb1bdae78a3b45f8c9058d5124a4815
Arthur Vie <arthur.vie@npl.co.uk>
2021-11-30 15:43:43 +0000

    Raw mode and get_line tests.

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,16 +1,70 @@
+#include <iostream>
+#include "terminal_control.h"
+#include "line_editor.h"
 
-typedef enum
+typedef enum test_names
 {
-    PLACEHOLDER
+    PLACEHOLDER,
+    ENABLE_RAW_MODE,
+    DISABLE_RAW_MODE,
+    RAW_MODE_SETTINGS,
+    GET_LINE
 } test_t;
 
-test_t current_test;
+test_t test = GET_LINE;
 
 int main()
 {
-    switch (current_test)
+    switch (test)
     {
         case PLACEHOLDER:
+        break;
+        
+        case ENABLE_RAW_MODE:
+        try
+        {
+            enable_raw_mode();
+        }
+        catch(std::runtime_error error)
+        {
+            std::cerr << error.what() << std::endl;
+        }
+        break;
+
+        case DISABLE_RAW_MODE:
+        try
+        {
+            enable_raw_mode();
+        }
+        catch(std::runtime_error error)
+        {
+            std::cerr << error.what() << std::endl;
+        }
+        break;
+
+        case RAW_MODE_SETTINGS:
+        try
+        {
+            enable_raw_mode();
+            disable_raw_mode();
+        }
+        catch(std::runtime_error error)
+        {
+            std::cerr << error.what() << std::endl;
+        }
+        break;
+
+        case GET_LINE:
+        try
+        {
+            enable_raw_mode();
+            std::cout << get_line();
+            disable_raw_mode();
+        }
+        catch(std::runtime_error error)
+        {
+            std::cerr << error.what() << std::endl;
+        }
         break;
 
         default:
