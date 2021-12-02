b3d04f7ab962b3e7a6fdcbff15ce2d740cc7f45e
JamilBen-Othman <95386987+JamilBen-Othman@users.noreply.github.com>
2021-12-02 16:25:40 +0000

    token_list pass by reference

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,30 +1,24 @@
-//#include "line_editor.h"
+#include "line_editor.h"
 #include <iostream>
 #include <string>
 #include <list>
+#include "tokenizer.h"
+
 
 //std::string command_line = get_line();
-std::string command_line = "command line example";
-
-enum item_types {COMMAND_WORD, NUMBER};
-
-struct Node {
-enum item_types item_type;
-std::string command_word;
-int number;
-std::string string;
-} ;
-
-std::list<struct Node> token_list;
-std::string place_holder;
+//std::string command_line = "command line example";
 
 
 
-int main()
+
+
+void tokenize(std::string command_line, std::list<struct Node> * token_list)
 {
+  std::string place_holder;
+
   int j = 0;
   std::list<struct Node>::iterator list_itr;
-  list_itr = token_list.begin();
+  list_itr = (*token_list).begin();
   //std::cout << command_line[0] << std::endl;
 
   for (int i=0 ; i<command_line.length() ; i++)
@@ -37,19 +31,8 @@ int main()
       struct Node new_node;
       new_node.string = place_holder;
       place_holder.clear();
-      token_list.push_back(new_node);
+      (*token_list).push_back(new_node);
       *list_itr = new_node;
-      //std::cout << j << std::endl;
-      
-      /*
-      while ( j < 10)
-        struct Node new_node;
-        new_node.string = place_holder;
-        place_holder.clear();
-        token_list.push_back(new_node);
-        *list_itr = new_node;
-        
-      */
     }
     else
     {
@@ -58,23 +41,13 @@ int main()
     
   }
   struct Node new_node;
-    new_node.string = place_holder;
-    place_holder.clear();
-    token_list.push_back(new_node);
-  //puts("here");
-  list_itr = token_list.begin();
-  for (list_itr = token_list.begin(); list_itr != token_list.end() ; list_itr ++)
-  {
-    //puts("here");
-    if (list_itr->string == "brown")
-    {
-      std::cout << list_itr->string << std::endl;
-    }
-    //please
-    std::cout << list_itr->string << std::endl;
-    
-    
+  new_node.string = place_holder;
+  place_holder.clear();
+  (*token_list).push_back(new_node);
 
+  for (list_itr = (*token_list).begin(); list_itr != (*token_list).end() ; list_itr ++)
+  { 
+    std::cout << list_itr->string << std::endl;
+    //peas
   }
-  //std::cout << place_holder << std::endl;
 }
