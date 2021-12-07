382ad00e5d0c9cc451e2c53848e4342689e857c9
chrisishakk <95387196+chrisishakk@users.noreply.github.com>
2021-12-07 18:59:49 +0000

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,27 +1,56 @@
-//please
-
-#include "tokenizer.h"
-#include "line_editor.h"
-#include "terminal_control.h"
 #include <iostream>
-#include <string>
 #include <list>
+#include<string> 
 
-int main()
-{
-  enable_raw_mode();
-  std::list <struct Node> token_list;
-  std::string command_line = get_line();
-  std::cout << command_line << std::endl;
-  tokenize(command_line, &token_list);
+int currentstate = 1 ;
+int NumbCount=0;
+int NumbLoc=0;
+int OpLoc=0;
+int OpCount=0;
 
-  std::cout << token_list.begin()->string << std::endl;
-  std::list<struct Node>::iterator list_itr;
+void parser(std::list<struct token_list_node> tokens){
+  std::list<struct token_list_node>::iterator my_iterator;
+my_iterator=tokens.begin();
 
-  for (list_itr = token_list.begin(); list_itr != token_list.end() ; list_itr ++)
-  { 
-    std::cout << list_itr->string << std::endl;
+for (int i=0;i<tokens.length(); i++){
+  if (my_iterator->is==NUMBER){
+    NumbCount++;
+  }else if(my_iterator->is==OPERATOR){
+    OpCount++;
   }
+  my_iterator++;//count how many numbers entered by user
+}
+my_iterator=tokens.begin();//reinitialize the iterator to the first token
+int *NumbArr=new int[NumbCount];//initialize dynamic array for numbs
+int *OpArr=new int[OpCount];//operators array
+for (int i=0;i<tokens.length(); i++){
+  if (my_iterator->is==NUMBER){
+    NumbArr[NumbLoc]=my_iterator->NUMBER;
+    NumbLoc++;
+  }else if(my_iterator->is==OPERATOR){
+    switch (my_iterator->OPERATOR){
+      case ADD:{
+        OpArr[OpLoc]=0;
+        break;
+      }case SUB:{
+        OpArr[OpLoc]=1;
+        break;
+      }case MUL:{
+        OpArr[OpLoc]=2;
+        break;
+      }case DIV:{
+        OpArr[OpLoc]=3;
+        if (my_iterator++->Number==0){
+          //error
+        }
+        break;
+      }case POW:{
+        OpArr[OpLoc]=4;
+        break;
+      }
+    }OpLoc++;
+  }
+  my_iterator++;
+}my_iterator=tokens.begin();
 
-  disable_raw_mode();
 }
