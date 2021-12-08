15a4f5fdfde21655b93ccce14bfa18a230cca58f
Arthur Vie <arthur.vie@npl.co.uk>
2021-12-08 10:23:31 +0000

    a map now used to link command words to their functions

--- tokenizer.h
+++ tokenizer.h
@@ -3,18 +3,103 @@
 
 #include <string>
 #include <list>
+#include <map>
+#include "functions_homogenised.h"
 
-enum item_types {COMMAND_WORD, NUMBER};
-
-struct Node
+enum operators
 {
-  enum item_types item_type;
-  std::string command_word;
-  int number;
-  std::string string;
-} ;
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLICATION,
+	DIVISION,
+	SQUARE,
+	SQUARE_ROOT,
+	EQUAL,
+	UNEQUAL,
+	LESS_THAN,
+	GREATER_THAN,
+	LESS_THAN_OR_EQUAL,
+	GREATER_THAN_OR_EQUAL,
+	OPEN_EXPRESSION,
+	CLOSE_EXPRESSION,
+	OPEN_BLOCK,
+	CLOSE_BLOCK,
+	SURPRES
+};
+// things to add: arrays? assignment operator? bitwise logical operations? pointers?
 
-void tokenize(std::string command_line, std::list <struct Node> * token_list);
+std::list<std::string> operators = {"+", "-", "*", "/", "^", "_/", "=", "/=", "<", ">", "<=", ">=", "(", ")", "{", "}"};
 
+enum separators
+{
+	OPEN_EXPRESSION,
+	CLOSE_EXPRESSION,
+	OPEN_COMMAND,
+	CLOSE_COMMAND
+};
+
+std::list<std::string> separators = {"(", ")", "[", "]"};
+
+struct command_info
+{
+	void (*function)(std::list<double> *);
+	std::string description;
+};
+
+std::map<std::string, struct command_info> command_map
+{
+	// arithmetic
+	{"add",			{&add,		"" }},
+	{"sub",			{&subtract,	"" }},
+	{"mul",			{&multiply,	"" }},
+	{"div",			{&divide,	"" }},
+	{"pow",			{&power,	"" }},	
+	{"mod",			{&modulus,	"" }},
+	{"abs",			{&absolute,	"" }},
+	// logical
+	{"and",			{&AND,		"" }},
+	{"or",			{&OR,		"" }},
+	{"not",			{&NOT,		"" }},
+	{"nor",			{&NOR,		"" }},
+	{"nand",		{&NAND,		"" }},
+	{"xor",			{&XOR,		"" }},
+	// stack manipulation
+	{"dup", 		{&dup,		"" }},
+	{"swap", 		{&swap,		"" }},
+	{"push", 		{&push,		"" }},
+	{"pop", 		{&pop,		"" }},
+	{"roll", 		{&roll,		"" }},
+	// system
+	{"cmd_list", 	{&cmd_list,	"" }},
+	{"help", 		{&help,		"" }},
+	{"quit", 		{&quit,		"" }},	
+	// other
+	{"react",		{&react,	"" }},
+	{"reluct",		{&reluct,	"" }},
+	{"power_IV",	{&power_IV,	"" }},
+	{"power_VR",	{&power_VR,	"" }}
+};
+
+enum token_type
+{
+		COMMAND_WORD,
+		NUMBER,
+		OPERATOR,
+		SEPARATOR
+};
+
+struct token_list_node
+{
+	enum token_type is;
+	std::string literal_string; // for debug only
+	unsigned int index;
+	unsigned int length;
+	struct command_info cmd_word;
+	double number;
+	enum operators op;
+	enum separators sep;
+};
+
+unsigned int RP_tokenize(std::string command_line, std::list<struct token_list_node> * token_list);
 
 #endif
