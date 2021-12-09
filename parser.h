1273bcbd3a74df9c6327ed5a00c7312052a9a86c
chrisishakk <95387196+chrisishakk@users.noreply.github.com>
2021-12-09 16:04:55 +0000

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,5 +1,5 @@
 #ifndef PARSER_H
-#define FUNCS_H
+#define PARSER_H
 
 void max_array(const int array[], int n,int &max,int &location);
 double parser(std::list<struct token_list_node> list of tokens)
