a1aeedff8a54f9ab85b2f143159c73b83d992322
Arthur Vie <arthur.vie@npl.co.uk>
2021-12-10 07:48:40 +0000

    moved function for printing token list to the tokenizer

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -13,6 +13,83 @@ struct line_section
 	std::string line;
 };
 
+void print_token_list(std::list<struct token_list_node> list)
+{
+	std::list<struct token_list_node>::iterator list_iter = list.begin();
+	while (list_iter != list.end())
+	{
+		switch (list_iter->is)
+		{
+			case COMMAND_WORD:
+			std::cout << list_iter->cmd_string << " -> ";
+			break;
+
+			case NUMBER:
+			std::cout << list_iter->number << " -> ";
+			break;
+
+			case OPERATOR:
+			switch (list_iter->op)
+			{
+				case ADD:
+				std::cout << '+';
+				break;
+
+				case SUB:
+				std::cout << '-';
+				break;
+
+				case MUL:
+				std::cout << '*';
+				break;
+
+				case DIV:
+				std::cout << '/';
+				break;
+
+				case POW:
+				std::cout << '^';
+				break;
+			
+				default:
+				break;
+			}
+			std::cout << " -> ";
+			break;
+
+			case SEPARATOR:
+			switch (list_iter->sep)
+			{
+				case OPEN_EXPRESSION:
+				std::cout << '(';
+				break;
+
+				case CLOSE_EXPRESSION:
+				std::cout << ')';
+				break;
+
+				case OPEN_COMMAND:
+				std::cout << '[';
+				break;
+
+				case CLOSE_COMMAND:
+				std::cout << ']';
+				break;
+			
+				default:
+				break;
+			}
+			std::cout << " -> ";
+			break;
+
+			default:
+			break;
+		}		
+		list_iter ++;
+	}
+	std::cout << std::endl;
+}
+
 unsigned int tokenize_RP(std::string line, std::list<struct token_list_node> * token_list)
 {
 	unsigned int i = 0;
--- tokenizer.h
+++ tokenizer.h
@@ -52,8 +52,10 @@ struct token_list_node
 //unsigned int find_numbers(std::string command_line, std::list<struct token_list_node> * token_list, unsigned int start_idx);
 //unsigned int find_command_words(std::string command_line, std::list<struct token_list_node> * token_list, unsigned int start_idx);
 
+void print_token_list(std::list<struct token_list_node> list);
 unsigned int tokenize_RP(std::string line, std::list<struct token_list_node> * token_list);
 unsigned int tokenize_infix(std::string line, std::list<struct token_list_node> * token_list);
+unsigned int tokenize(std::string line, std::list<struct token_list_node> * token_list);
 
 //unsigned int RP_tokenize(std::string command_line, std::list<struct token_list_node> * token_list);
 
